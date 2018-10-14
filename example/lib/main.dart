3a51dc4444b9c567d82d4198436aa552677fb7ec
Pawel Polanski <jaggernod@gmail.com>
2018-10-14 16:34:42 +0200

    Removed not used import

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_stetho/flutter_stetho.dart';
-import 'package:http/http.dart' as http;
 
 void main() {
   Stetho.initialize();
